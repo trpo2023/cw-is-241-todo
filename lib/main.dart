57c4b2f923785092c2ac162d6880683a2b71b4db
Skillzes <124029368+Skillzes@users.noreply.github.com>
2023-05-03 15:34:25 +0700

    add main.dart
    add main.dart

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TodoListApp());
+}
+
+class TodoListApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: TodoListPage(),
+    );
+  }
+}
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+class _TodoListPageState extends State<TodoListPage> {
+  final _textController = TextEditingController();
+  final _items = <String>[];
+
+  void _addTodoItem() {
+    setState(() {
+      final todoItem = _textController.text;
+      if (todoItem.isNotEmpty) {
+        _items.add(todoItem);
+        _textController.clear();
+      }
+    });
+  }
+
+  void _removeTodoItem(int index) {
+    setState(() {
+      _items.removeAt(index);
+    });
+  }
+
+  void _editTodoItem(String newValue, int index) {
+    setState(() {
+      _items[index] = newValue;
+    });
+  }
+
+  Widget _buildTodoItem(String item, int index) {
+    return Dismissible(
+      key: Key('$item$index'),
+      onDismissed: (direction) => _removeTodoItem(index),
+      child: ListTile(
+        title: Text(item),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            IconButton(
+              icon: Icon(Icons.edit),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    final TextEditingController controller =
+                    TextEditingController(text: item);
+                    return AlertDialog(
+                      title: Text('Edit a task'),
+                      content: TextField(
+                        controller: controller,
+                      ),
+                      actions: [
+                        TextButton(
+                          child: Text('Cancel'),
+                          onPressed: () => Navigator.pop(context),
+                        ),
+                        TextButton(
+                          child: Text('Save'),
+                          onPressed: () {
+                            _editTodoItem(controller.text, index);
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () => _removeTodoItem(index),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTodoList() {
+    return ListView.builder(
+      itemCount: _items.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildTodoItem(_items[index], index);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              hintText: 'Enter a task',
+            ),
+            onSubmitted: (value) {
+              _addTodoItem();
+            },
+          ),
+          Expanded(
+            child: _buildTodoList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
