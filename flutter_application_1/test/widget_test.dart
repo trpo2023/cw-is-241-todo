9019594e98b17ecab939bfcf783c731a939ede37
Andrew zhur <gosharak12345@mail.ru>
2023-05-22 21:02:40 +0700

    Add new element test

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -10,3 +10,29 @@ void main() {
     // Проверяем, что заголовок приложения отображается
     expect(find.text('Todo List'), findsOneWidget);
   });
+
+  testWidgets('Add new todo item', (WidgetTester tester) async {
+    // Создаем экземпляр нашего виджета
+    await tester.pumpWidget(TodoListApp());
+
+    // Находим кнопку добавления задачи и нажимаем на нее
+    await tester.tap(find.byType(TextField));
+    await tester.pumpAndSettle();
+
+    // Проверяем, что появился диалоговое окно
+    expect(find.byType(AlertDialog), findsOneWidget);
+
+    // Вводим текст в текстовое поле и нажимаем на кнопку "Add"
+    await tester.tap(find.byType(TextField).last);
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(TextField).last, 'New Task');
+    final textn = find.text('New Task');
+    await tester.tap(find.text('Add'));
+    await tester.pumpAndSettle();
+
+    // Проверяем, что новая задача добавлена на экран
+    expect(textn, findsOneWidget);
+  });
+
+}
