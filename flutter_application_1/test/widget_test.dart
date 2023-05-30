ebceca56a773d7acd4c5a8d6bd1918b5b86c2a29
Andrew zhur <gosharak12345@mail.ru>
2023-05-30 15:13:03 +0700

    Update tests for new main.c

--- flutter_application_1/test/widget_test.dart
+++ flutter_application_1/test/widget_test.dart
@@ -1,121 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_application_1/main.dart';
+import 'package:todo_repository/todo_repository.dart';
+import 'package:todo_models/todo_model.dart';
 
 void main() {
-  testWidgets('Todo List App', (WidgetTester tester) async {
-    // Запускаем приложение
-    await tester.pumpWidget(TodoListApp());
-
-    // Проверяем, что заголовок приложения отображается
-    expect(find.text('Todo List'), findsOneWidget);
+  group('TodoListApp', () {
+    testWidgets('renders MaterialApp', (tester) async {
+      await tester.pumpWidget(TodoListApp());
+      expect(find.byType(MaterialApp), findsOneWidget);
+    });
   });
 
-  testWidgets('Add new todo item', (WidgetTester tester) async {
-    // Создаем экземпляр нашего виджета
-    await tester.pumpWidget(TodoListApp());
+  group('TodoListPage', () {
+    testWidgets('renders AppBar', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: TodoListPage()));
+      expect(find.byType(AppBar), findsOneWidget);
+    });
 
-    // Находим кнопку добавления задачи и нажимаем на нее
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-
-    // Проверяем, что появился диалоговое окно
-    expect(find.byType(AlertDialog), findsOneWidget);
-
-    // Вводим текст в текстовое поле и нажимаем на кнопку "Add"
-    await tester.tap(find.byType(TextField));
-    await tester.pumpAndSettle();
-
-    await tester.enterText(find.byType(TextField), 'New Task');
-    final textn = find.text('New Task');
-    await tester.tap(find.text('Add'));
-    await tester.pumpAndSettle();
-
-    // Проверяем, что новая задача добавлена на экран
-    expect(textn, findsOneWidget);
-  });
-
-  testWidgets('Edit a task', (WidgetTester tester) async {
-    await tester.pumpWidget(TodoListApp());
-
-    // Создаем экземпляр нашего виджета
-    await tester.pumpWidget(TodoListApp());
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    await tester.tap(find.byType(TextField));
-    await tester.pumpAndSettle();
-    await tester.enterText(find.byType(TextField), 'New Task');
-    await tester.tap(find.text('Add'));
-    await tester.pumpAndSettle();
-
-    // Нажимаем на кнопку редактирования задачи
-    await tester.tap(find.byIcon(Icons.edit).first);
-    await tester.pumpAndSettle();
-
-    // Проверяем, что появился диалоговое окно
-    expect(find.byType(AlertDialog), findsOneWidget);
-
-    // Вводим новое название задачи
-    await tester.enterText(find.byType(TextField).last, 'Edited Task');
-    await tester.pump();
-
-    // Нажимаем на кнопку сохранения задачи
-    await tester.tap(find.text('Save'));
-    await tester.pumpAndSettle();
-
-    // Проверяем, что задача была отредактирована
-    expect(find.text('Edited Task'), findsOneWidget);
-  });
-
-    testWidgets('Delete a task', (WidgetTester tester) async {
-
-    await tester.pumpWidget(TodoListApp());
+    testWidgets('renders FloatingActionButton', (tester) async {
+      await tester.pumpWidget(MaterialApp(home: TodoListPage()));
+      expect(find.byIcon(Icons.add), findsOneWidget);
+    });
+  })
     
-    // Создаем экземпляр нашего виджета
-    await tester.pumpWidget(TodoListApp());
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    await tester.tap(find.byType(TextField));
-    await tester.pumpAndSettle();
-    await tester.enterText(find.byType(TextField), 'New Task');
-    final textn = find.text('New Task');
-    await tester.tap(find.text('Add'));
-    await tester.pumpAndSettle();
-    
-    expect(textn, findsOneWidget);
-
-    await tester.tap(find.byIcon(Icons.delete).first);
-    await tester.pumpAndSettle();
-
-    // Проверяем, что задача была удалена
-    expect(find.text('New Task'), findsNothing);
-
-  });
-
-    testWidgets('Delete a task by left swipe', (WidgetTester tester) async {
-
-    await tester.pumpWidget(TodoListApp());
-
-     // Создаем экземпляр нашего виджета
-    await tester.pumpWidget(TodoListApp());
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
-    await tester.tap(find.byType(TextField));
-    await tester.pumpAndSettle();
-    await tester.enterText(find.byType(TextField), 'New Task');
-    final textn = find.text('New Task');
-    await tester.tap(find.text('Add'));
-    await tester.pumpAndSettle();
-
-    expect(textn, findsOneWidget);
-
-    final firstItemFinder = textn;
-    final DismissDirection swipeDirection = DismissDirection.startToEnd;
-    await tester.drag(firstItemFinder, Offset(-1000.0, 0.0));
-    await tester.pumpAndSettle();
-
-    // Проверяем, что задача была удалена
-    expect(find.text('New Task'), findsNothing);
-
-  });
 }
